555a134cc4602d2f9bbbddcbf1b57657c87ae7dd
Lucas Garcia <lucasugarcia@gmail.com>
2021-09-26 19:52:42 -0300

    Projeto desenvolvido durante o curso

---
+++ ByteBank.Forum/App_Start/Identity/MailAdress.cs
@@ -0,0 +1,11 @@
+﻿using System.Net.Mail;
+
+namespace ByteBank.Forum.App_Start.Identity
+{
+    internal class MailAdress : MailAddress
+    {
+        public MailAdress(string address) : base(address)
+        {
+        }
+    }
+}
